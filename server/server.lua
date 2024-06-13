de71d9f07f0565e88559fee40c66eb33cd360b2b
Diogo Gramunha Silva <128035019+DiogoGramunha@users.noreply.github.com>
2024-06-13 04:02:09 +0100

    Updated server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,14 +3,12 @@ local ESX = exports["es_extended"]:getSharedObject()
 RegisterNetEvent('yoda-chopshop:RentVeh', function()
     local xPlayer = ESX.GetPlayerFromId(source)
     local rentVeh = false
-    
-    if xPlayer.getMoney() >= Config.Context.value then
-        xPlayer.removeMoney(Config.Context.value)
+    if xPlayer.getInventoryItem('money').count >= Config.Context.value then
+        xPlayer.removeInventoryItem("money", Config.Context.value)
         rentVeh = true
     elseif xPlayer.getAccount('bank').money >= Config.Context.value then
         xPlayer.removeAccountMoney('bank', Config.Context.value)
         rentVeh = true
     end
-
     TriggerClientEvent('yoda-chopshop:RentVehResponse', source, rentVeh)
 end)
