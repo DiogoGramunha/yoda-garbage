b1f3bb043405a427bfd5ffcc4aeb52afd3172175
Diogo Gramunha Silva <128035019+DiogoGramunha@users.noreply.github.com>
2024-06-29 02:51:50 +0100

    Update client.lua
    Duplicated truck spawn

--- server/server.lua
+++ server/server.lua
@@ -1,9 +1,10 @@
-local ESX = exports["es_extended"]:getSharedObject()
+local ESX = nil
 
-AddEventHandler('yoda-garbage:RentVeh')
-RegisterNetEvent('yoda-garbage:RentVeh', function()
+RegisterNetEvent('yoda-garbage:RentVeh')
+AddEventHandler('yoda-garbage:RentVeh', function()
     local xPlayer = ESX.GetPlayerFromId(source)
     local rentVeh = false
+
     if xPlayer.getInventoryItem('cash').count >= Config.Context.value then
         xPlayer.removeInventoryItem("cash", Config.Context.value)
         rentVeh = true
@@ -11,19 +12,17 @@ RegisterNetEvent('yoda-garbage:RentVeh', function()
         xPlayer.removeAccountMoney('bank', Config.Context.value)
         rentVeh = true
     end
+
     TriggerClientEvent('yoda-garbage:RentVehResponse', source, rentVeh)
 end)
 
-
-RegisterNetEvent('yoda-garbage:getPayment', function(payment, binsDeposited)
-    print('Evento yoda-garbage:getPayment acionado.')
+RegisterNetEvent('yoda-garbage:getPayment')
+AddEventHandler('yoda-garbage:getPayment', function(payment, binsDeposited)
     local _source = source
     if binsDeposited and binsDeposited > 0 then 
         local totalPayment = (payment * binsDeposited) + Config.Context.value
-        exports.ox_inventory:AddItem(_source, 'cash', totalPayment)
         TriggerClientEvent('yoda-garbage:Payment', _source, totalPayment)
     else
         TriggerClientEvent('yoda-garbage:paymentFail', _source)
-        exports.ox_inventory:AddItem(_source, 'cash', Config.Context.value)
     end
 end)
